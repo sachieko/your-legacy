11763310157fbb8c311ab967139f7cf1e0cf20be
sachieko <arashikabane@gmail.com>
2022-10-20 16:40:26 -0600

    Initialize react app, strip out unwanted defaults

---
+++ legacy-front-end/src/App.js
@@ -0,0 +1,12 @@
+
+const App = function() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p> Empty it out! </p>
+      </header>
+    </div>
+  );
+};
+
+export default App;
