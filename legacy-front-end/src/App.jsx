10b5c9b05cde1f6e397b54e9dc983a74f82215f3
sachieko <arashikabane@gmail.com>
2022-10-21 05:17:03 -0600

    rename to JSX file

--- legacy-front-end/src/App.jsx
+++ legacy-front-end/src/App.jsx
@@ -1,9 +1,11 @@
+import AbilityScore from "./components/CharacterSheet/AbilityScore";
 
 const App = function() {
   return (
     <div className="App">
       <header className="App-header">
         <p> Empty it out! </p>
+        <AbilityScore value='10' />
       </header>
     </div>
   );
